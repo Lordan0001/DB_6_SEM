5ba65c46a5e4291252b8b03fa618681b682241b6
Vladislav <70939188+Lordan0001@users.noreply.github.com>
2023-02-17 22:21:31 +0300

    Create Views.sql

---
+++ LAB_1/Views.sql
@@ -0,0 +1,20 @@
+use SixSemStore;
+select * from Orders;
+select * from Customers;
+
+Create or alter VIEW FullOrderInfo AS
+select Customers.customerName, Customers.customerEmail, Customers.creditBalance,
+Orders.orderdProduct, Orders.orderDate, Orders.orderedCount
+from Customers inner JOIN Orders on Customers.customerName = Orders.client;
+
+Select * from FullOrderInfo;
+
+Create or Alter VIEW ShowGoods AS
+select * from Goods;
+
+Select * from ShowGoods;
+
+Create or Alter VIEW ShowAccounts AS
+select * from Accounts;
+
+Select * from ShowAccounts;
