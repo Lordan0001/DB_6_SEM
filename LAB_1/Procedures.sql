eee36661f9894a8a36f534c043eae55cd7c9a196
Vladislav <70939188+Lordan0001@users.noreply.github.com>
2023-02-18 12:37:15 +0300

    Procedures -done

--- LAB_1/Procedures.sql
+++ LAB_1/Procedures.sql
@@ -58,3 +58,85 @@ exec MakeOrder 'Motorhead','John Wick','01-01-2023',5
 select * from Goods;
 Select * from Customers;
 select * from Orders;
+
+
+
+go
+Create or alter procedure RemoveOrder
+@OrProduct nvarchar(254),
+@Client nvarchar(254)
+as begin
+DECLARE
+@OldCount int,
+@NewCount int,
+@OrCount int
+begin try
+SET @OldCount = (Select [count] from Goods where productName = @OrProduct);
+SET @OrCount = (Select orderedCount from Orders where orderedProduct = @OrProduct) 
+SET @NewCount = @OldCount + @OrCount;
+
+Delete from Orders where orderedProduct = @OrProduct AND client = @Client;
+
+Update Goods SET [count] = @NewCount where productName = @OrProduct;
+print 'Order delted!';
+end try
+begin catch
+print 'Error: ' + cast(error_number() as varchar(6)) + ': ' + error_message();
+end catch
+end;
+go
+
+exec RemoveOrder 'Motorhead','John Wick';
+
+
+go
+Create or alter procedure AddFullUser
+@CustomerName nvarchar(254),  -- main name
+@Email nvarchar(254),
+@CreditBalance money,
+@Login nvarchar(254),
+@Password nvarchar(254)
+
+as begin
+
+begin try
+
+Insert INTO Accounts([login],[password],[name],email)
+Values(@Login,@Password,@CustomerName,@Email);
+
+Insert Into Customers(customerName,customerEmail,creditBalance)
+Values(@CustomerName,@Email,@CreditBalance);
+print 'User Created!'
+
+end try
+begin catch
+print 'Error: ' + cast(error_number() as varchar(6)) + ': ' + error_message();
+end catch
+end;
+go
+
+Exec AddFullUser 'TEstName','test@mail.com',6000,'testLogin','testPass'
+
+select * from Customers;
+select * from Accounts;
+
+go
+Create or alter procedure DeleteFullUser
+@CustomerName nvarchar(254)  -- main name
+
+as begin
+
+begin try
+
+delete from Customers where customerName = @CustomerName;
+delete from Accounts where [name] = @CustomerName;
+print 'User Deleted!'
+
+end try
+begin catch
+print 'Error: ' + cast(error_number() as varchar(6)) + ': ' + error_message();
+end catch
+end;
+go
+
+exec DeleteFullUser 'TEstName'
